887286155923385a2fa28a4688647c98611724d6
AstrA144 <ggastra1409@gmail.com>
2025-10-15 17:00:58 +0500

    dz 1
    dz 1

--- ggastra14/main.py
+++ ggastra14/main.py
@@ -1,1 +1,24 @@
-print("hello egr")
+a = int(input("число 1: "))
+b = int(input("число 2: "))
+c = int(input("число 3: "))
+print(a + b + c)
+
+
+
+a = int(input("зарплата за месяц: "))
+b = int(input("месячный платёж: "))
+c = int(input("комунальные услуги: "))
+print(a - b - c )
+
+
+d1 = int(input("Длинна 1 диагонали: "))
+d2 = int(input("длинна 2 диагонали: "))
+s = (d1 * d2) / 2
+print(s)
+
+
+print("to be \nor not \nto be")
+
+
+print('"Life is what happens \n                      when \n                             youre busy making other plans"   ')
+print('                                                              John Lennon')
